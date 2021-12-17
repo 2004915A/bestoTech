9a92178abd7768a5f9612791f1928bd95cf24d90
TANG JUN HONG 2001873E <2001873E@student.tp.edu.sg>
2021-12-18 00:38:33 +0800

    test

---
+++ bestoTech/Shared/Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bestoTech.Shared.Domain
+{
+    public class Product : BaseDomainModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int AverageRating { get; set; }
+        public virtual Brand Brand { get; set; }
+        public virtual ProductCategory ProductCategory { get; set; } 
+    }
+}
---
+++ bestoTech/Shared/Domain/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bestoTech.Shared.Domain
+{
+    public class Review : BaseDomainModel
+    {
+        public int ReviewId { get; set; }
+        public int ProductId  { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewDescription { get; set; }
+        public int ReviewRating { get; set; }
+        public string ReviewStatus { get; set; }
+        public int RecieptId { get; set; }
+    }
+}
