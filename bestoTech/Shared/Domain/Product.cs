b310eff10e98a74930d0b27a4508f7694b1b4721
Adib <92657586+2004915A@users.noreply.github.com>
2021-12-18 21:47:05 +0800

    test

--- bestoTech/Shared/Domain/Product.cs
+++ bestoTech/Shared/Domain/Product.cs
@@ -9,12 +9,15 @@ namespace bestoTech.Shared.Domain
     public class Product : BaseDomainModel
     {
         public int ProductId { get; set; }
-        public string Name { get; set; }
+        public string ProductName { get; set; }
         public string Description { get; set; }
         public float AvgRating { get; set; } 
-        public string BrandId { get; set; }
+        public int TotalReviews { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
         public virtual Brand Brand { get; set; }
         public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
         public virtual Category Category { get; set; } 
     }
 }
