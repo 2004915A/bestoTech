d9faff649b710ec62be5ea679354c75be419a290
TANG JUN HONG 2001873E <2001873E@student.tp.edu.sg>
2021-12-18 23:20:36 +0800

    BrandSeed2

--- bestoTech/Server/Configurations/Entities/BrandSeedConfig.cs
+++ bestoTech/Server/Configurations/Entities/BrandSeedConfig.cs
@@ -16,12 +16,10 @@ namespace bestoTech.Server.Configurations.Entities
                 new Brand
                 {
                     Id = 1,
-                    BrandId = 1,
-                    BrandName = "Sony",
+                    Name = "Sony",
                     NumOfProduct = 1, 
                     CategoryId = 1, 
                     CategoryName = "",
-                    ProductName = "", 
                     AvgRating = 0,
 
 
@@ -33,12 +31,10 @@ namespace bestoTech.Server.Configurations.Entities
                  new Brand
                  {
                      Id = 2,
-                     BrandId = 2,
-                     BrandName = "Apple",
+                     Name = "Apple",
                      NumOfProduct = 2,
                      CategoryId = 2,
-                     CatergoryName = " ",
-                     ProductName = "",
+                     CategoryName = "",
                      AvgRating = 0,
 
                      DateCreated = DateTime.Now,
@@ -49,12 +45,10 @@ namespace bestoTech.Server.Configurations.Entities
                  new Brand
                  {
                      Id = 3,
-                     BrandId = 3,
-                     BrandName = "Samsung",
+                     Name = "Samsung",
                      NumOfProduct = 3,
                      CategoryId = 3,
                      CategoryName = " ",
-                     ProductName = "",
                      AvgRating = 0,
         
 
