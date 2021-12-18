da5617ab03aa637fd2a1d991e8e00fc76a603cfd
TANG JUN HONG 2001873E <2001873E@student.tp.edu.sg>
2021-12-18 21:47:40 +0800

    test

--- bestoTech/Server/Configurations/Entities/CategorySeedConfig.cs
+++ bestoTech/Server/Configurations/Entities/CategorySeedConfig.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using bestoTech.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace bestoTech.Server.Configurations.Entities
 {
-    public class CategorySeedConfig
+    public class CategorySeedConfig : IEntityTypeConfiguration<Category>
     {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+
+                new Category
+                {
+                    Id = 1,
+                    Name = "Smart Phones",
+
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Audio and Visual",
+
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Cameras",
+
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                });
+        }
     }
 }
