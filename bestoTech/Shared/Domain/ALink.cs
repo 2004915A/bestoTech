bfcf6222533fa98bba09db4951902cd064ac6042
Adib <92657586+2004915A@users.noreply.github.com>
2022-02-08 02:06:34 +0800

    Form Validation and View Pages

--- bestoTech/Shared/Domain/ALink.cs
+++ bestoTech/Shared/Domain/ALink.cs
@@ -12,8 +12,12 @@ namespace bestoTech.Shared.Domain
     public class ALink 
     {
         public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Url)]
         public string Link { get; set; }
+        [DataType(DataType.Currency)]
         public int? Price { get; set; }
+        [Required(ErrorMessage = "Please select a Product")]
         [ForeignKey("ProductId")]
         public int? ProductId { get; set; }
         public virtual Product Product { get; set; }
--- bestoTech/Shared/Domain/Product.cs
+++ bestoTech/Shared/Domain/Product.cs
@@ -11,10 +11,14 @@ namespace bestoTech.Shared.Domain
     public class Product 
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a Name"), MaxLength(20), MinLength(3)]
         public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter a description"), MaxLength(500), MinLength(10)]
         public string Description { get; set; }
+        [Required]
         public float AvgRating { get; set; } 
         public int? TotalReviews { get; set; }
+        [Required]
         [ForeignKey("Brand")]
         public int? BrandId { get; set; }
         public virtual Brand Brand { get; set; }
